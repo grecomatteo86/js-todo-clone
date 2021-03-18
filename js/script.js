e7e5e74bec7b2556d579f6774c3d9a17445cc726
Matteo <grecomatteo86@gmail.com>
2021-03-18 16:04:41 +0100

    definizione markup e bucket list

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* Creare una todo list usando un template */
+
+/* 1. Definisco una lista di cose da fare con un array */
+
+var bucketList = [
+
+  'Inventare un profumo',
+  'Progettare un videogioco 8 bit',
+  'Tatuarmi la madonna sulla schiena',
+
+];
