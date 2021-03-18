7f8ddb3c23c4d575ba8fd0267f6ad7cb766994a7
Matteo <grecomatteo86@gmail.com>
2021-03-18 16:43:11 +0100

    append template in html

--- js/script.js
+++ js/script.js
@@ -17,6 +17,7 @@ for ( var i = 0; i < bucketList.length; i++) {
   var template = $('.template li').clone();
   /* 5. Aggiungo il listato al template */
   template.prepend(bucketList[i]);
-  
+  /* 6. Prendo il mio template e lo metto nell'html */
+  $('.bucket_list').append(template);
 
 }
